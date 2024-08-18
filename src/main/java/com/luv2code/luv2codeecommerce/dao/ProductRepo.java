9516ee268b07f9108eb2b287fbcc90034ad91db0
chigozieFrancis <chigozie-cyriacus.francis@iu-study.org>
2024-08-18 10:28:36 +0200

    Initial Commits

--- src/main/java/com/luv2code/luv2codeecommerce/dao/ProductRepo.java
+++ src/main/java/com/luv2code/luv2codeecommerce/dao/ProductRepo.java
@@ -2,6 +2,8 @@ package com.luv2code.luv2codeecommerce.dao;
 
 import com.luv2code.luv2codeecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product, Long> {
 }
