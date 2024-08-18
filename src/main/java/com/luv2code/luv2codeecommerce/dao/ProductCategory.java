804103c0b35845167118cf052e3a366053ad1b21
chigozieFrancis <chigozie-cyriacus.francis@iu-study.org>
2024-08-18 11:01:31 +0200

    Initial Commits

--- src/main/java/com/luv2code/luv2codeecommerce/dao/ProductCategory.java
+++ src/main/java/com/luv2code/luv2codeecommerce/dao/ProductCategory.java
@@ -3,6 +3,6 @@ package com.luv2code.luv2codeecommerce.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategory extends JpaRepository<com.luv2code.luv2codeecommerce.entity.ProductCategory, Long> {
 }
